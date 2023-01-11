6d13351b9f99ed13b8077a554cd62e57bbc36848
Kalama Kiefer <kalamakiefer@gmail.com>
2023-01-11 12:22:04 -1000

    feat: clip corners of greeting

--- apps/site/src/components/Greeting.tsx
+++ apps/site/src/components/Greeting.tsx
@@ -12,8 +12,8 @@ export const Greeting = () => {
 				<Dialog.Root open={openInstructions}>
 					<Dialog.Overlay className="fixed inset-0 bg-black opacity-75" />
 					<Dialog.Content className="fixed inset-4 lg:inset-y-28 focus:outline-none flex items-center justify-center">
-						<div className="flex flex-col max-w-[650px] p-4 bg-black">
-							<div className="pt-10 pb-4 px-6 lg:px-[99px] lg:pt-[97px] lg:pb-[40px] text-center flex items-center flex-col bg-body">
+						<div className="flex flex-col max-w-[650px] p-4 bg-black rounded-xl">
+							<div className="relative overflow-hidden pt-10 pb-4 px-6 lg:px-[99px] lg:pt-[97px] lg:pb-[40px] text-center flex items-center flex-col bg-body clip-corners">
 								<p className="font-bold text-xl md:text-30 leading-tight capsize uppercase">
 									Step 1
 								</p>
@@ -58,13 +58,13 @@ export const Greeting = () => {
 				<Dialog.Root defaultOpen>
 					<Dialog.Overlay className="fixed inset-0 bg-black opacity-75" />
 					<Dialog.Content className="fixed inset-4 lg:inset-y-28 focus:outline-none flex items-center justify-center">
-						<div className="flex flex-col max-w-[650px] p-4 bg-black">
-							<div className="flex items-center justify-center bg-header pb-[31px] pt-[17px] px-4">
+						<div className="flex flex-col max-w-[650px] p-4 bg-black rounded-xl">
+							<div className="relative overflow-hidden flex items-center justify-center bg-header pb-[31px] pt-[17px] px-4 clip-t-corners">
 								<MobileHeader className="w-full max-w-[369px]" />
 							</div>
 							<div className="bg-orange-main h-5" />
 							<div className="bg-yellow-search h-5" />
-							<div className="py-6 px-6 lg:px-[50px] lg:pt-[32px] lg:pb-[26px] text-center flex items-center flex-col bg-body">
+							<div className="relative overflow-hidden py-6 px-6 lg:px-[50px] lg:pt-[32px] lg:pb-[26px] text-center flex items-center flex-col bg-body clip-b-corners">
 								<p className="font-medium text-sm md:text-22 leading-tight capsize">
 									At Wall-to-Wall Studios some of us like mixtapes, and some of
 									us are too young to understand what this year's Occasional
